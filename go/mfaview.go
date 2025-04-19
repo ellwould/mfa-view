dbf97298d907e60a296fb51bbc370f8de92372fd
Elliot Michael Keavney <elliotkeavney@protonmail.com>
2025-04-19 10:15:57 +0100

    Added function to draw message boxes on CLI, added more comments to explain code better and changed function that validates user input to support different validation from various types of input

--- go/mfaview.go
+++ go/mfaview.go
@@ -11,19 +11,24 @@ import (
 	"os"
 	"slices"
 	"time"
+	"strings"
+	"strconv"
 )
 
-// American National Standards Institute (ANSI) reset colour code
+// Variable for American National Standards Institute (ANSI) reset colour code
 var resetColour = "\033[0m"
 
-// American National Standards Institute (ANSI) text colour codes
+// Variables for American National Standards Institute (ANSI) text colour codes
 var textBoldWhite = "\033[1;37m"
 var textBoldBlack = "\033[1;30m"
 
-// American National Standards Institute (ANSI) background colour codes
+// Variables for American National Standards Institute (ANSI) background colour codes
 var bgRed = "\033[41m"
 var bgGreen = "\033[42m"
 var bgYellow = "\033[43m"
+var bgBlue = "\033[44m"
+var bgPurple = "\033[45m"
+var bgCyan = "\033[46m"
 
 // Clear screen function for GNU/Linux OS's
 func clearScreen() {
@@ -54,11 +59,21 @@ func inputForm(w http.ResponseWriter, value string, labelMessage string, inputTy
 	fmt.Fprintf(w, "<br>")
 }
 
-// Function to validate user input
-func validateInput(formValue string, valueType string, minNum string, maxNum string) (validation bool) {
+// Function to validate user input utlising the Go Validator package
+func validateInput(formValue string, valueType string) (validation bool) {
 	validateInput := validator.New()
-	if valueType == "" {
-		validateInputErr := validateInput.Var(formValue, "required,min="+minNum+",max="+maxNum)
+	// Conditional statments are used for each type of value inputted from a user
+	if valueType == "email" {
+                validateInputErr := validateInput.Var(formValue, "email,required,min=6,max=320")                
+                if validateInputErr != nil {
+                	validation = false
+                	return
+		} else {
+			validation = true
+			return
+		}
+	} else if valueType == "password" {
+		validateInputErr := validateInput.Var(formValue, "required,min=20,max=100")
 		if validateInputErr != nil {
 			validation = false
 			return
@@ -66,15 +81,27 @@ func validateInput(formValue string, valueType string, minNum string, maxNum str
 			validation = true
 			return
 		}
+	} else if valueType == "2FA" {
+		validateInputErr := validateInput.Var(formValue, "number,required,min=6,max=6")
+		if validateInputErr != nil {
+			validation = false
+			return
+		} else {
+			validation = true
+			return
+		}
+        } else if valueType == "text" {
+                validateInputErr := validateInput.Var(formValue, "required,min=10,max=200")
+                if validateInputErr != nil {
+                        validation = false
+                        return
+                } else {
+                        validation = true
+                        return
+                }
 	} else {
-		validateInputErr := validateInput.Var(formValue, valueType+",required,min="+minNum+",max="+maxNum)
-		if validateInputErr != nil {
-			validation = false
-			return
-		} else {
-			validation = true
-			return
-		}
+		validation = false
+		return
 	}
 }
 
@@ -104,87 +131,177 @@ func genPasswd(passwd []byte) string {
 	return string(hashAndSalt)
 }
 
-// Recursive function to add a user
-// Needs alot of work this function:
-func createUser() {
+// Function to draw box with squares around message, must have a message with characters that total a odd number
+func messageBoxCli(bgColour string, messageColour string, message string) {
+	topBottomSquare := strings.Repeat(" □", (len(message)/2)+6)
+	inbetweenSpace := strings.Repeat(" ", len(message)+8)
+ 	fmt.Println(bgColour + messageColour)
+	fmt.Println(topBottomSquare + " ")
+	fmt.Println(" □" + inbetweenSpace + "□ ")
+	fmt.Println(" □    "+message+"    □ ")
+	fmt.Println(" □" + inbetweenSpace + "□ ")
+	fmt.Println(topBottomSquare + " ")
+	fmt.Print(resetColour)
+}
+
+// Function to display message on CLI informing the user the configuration file has a wrong value
+func invalidEnvCli(message string) {
 	clearScreen()
-	fmt.Println(bgGreen + textBoldWhite)
-	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
-	fmt.Println(" □                                                       □ ")
-	fmt.Println(" □               email address is required               □ ")
-	fmt.Println(" □                                                       □ ")
-	fmt.Println(" □   email address can be changed later in mfaview.env   □ ")
-	fmt.Println(" □                                                       □ ")
-	fmt.Println(" □            (Type \"exit\" to quit program)            □ ")
-	fmt.Println(" □                                                       □ ")
-	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
-	fmt.Println(resetColour)
+	messageBoxCli(bgRed, textBoldWhite, message)
+	os.Exit(0)
+}
+
+// Function to terminate the program with exit code 0 to indicate there were no errors
+func exitProgramCli() {
+	clearScreen()
+	messageBoxCli(bgBlue, textBoldWhite, "Program exited.")
+	os.Exit(0)
+}
+
+// Function to inform the user of wrong input and waits for user to press enter/return key
+func invalidInputCli() {
+	clearScreen()
+	messageBoxCli(bgRed, textBoldWhite, "Invalid input press enter/return to continue.")
+        fmt.Scanln()
+}
+
+// Function to inform the user to type exit to terminate the program
+func typeExitCli() {
+	messageBoxCli(bgBlue, textBoldWhite, "(Type exit to quit program)")
+}
+
+// A recursive function to add a user email
+func addEmailCli() {
+	clearScreen()
+	typeExitCli()
+	messageBoxCli(bgCyan, textBoldWhite, "Email address is required")
+        messageBoxCli(bgRed, textBoldWhite, "Email address can be manually changed later in /usr/local/etc/mfaview/env/mfaview.env")
+        fmt.Println(textBoldBlack)
+        fmt.Printf("   Please enter an email address between 6-320\n   characters: ")
+        var addEmail string
+        fmt.Scan(&addEmail)
+        fmt.Println(resetColour)
+        validationAddEmail := validateInput(addEmail, "email")
+	if addEmail == "exit" || addEmail == "Exit" || addEmail == "EXIT" {
+		exitProgramCli()
+	} else if validationAddEmail == false {
+		invalidInputCli()
+		addEmailCli()
+	} else {
+		fmt.Println(addEmail)
+	}	
+}
+
+// A recursive function to add a user password, a hashed and salted value of the password will be stored
+func addPasswordCli() {
+	clearScreen()
+	typeExitCli()
+	messageBoxCli(bgPurple, textBoldWhite, "A password is required ")
 	fmt.Println(bgRed + textBoldWhite)
 	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
-	fmt.Println(" □                                                                                                               □ ")
-	fmt.Println(" □                                            A password is required                                             □ ")
 	fmt.Println(" □                                                                                                               □ ")
 	fmt.Println(" □                           The password stored on the disk will be hashed and salted                           □ ")
 	fmt.Println(" □   The password will be used to encrypt & decrypt Multi-factor authentication (MFA) secret keys on the disk.   □ ")
 	fmt.Println(" □     A lost password will result in unreadable Multi-factor authentication (MFA) secret keys on the disk!      □ ")
 	fmt.Println(" □      Store the password securly, do not lose the password, there is no way to recover a lost password!!       □ ")
 	fmt.Println(" □                                                                                                               □ ")
-	fmt.Println(" □                                        (Type \"exit\" to quit program)                                        □ ")
-	fmt.Println(" □                                                                                                               □ ")
 	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
-	fmt.Println(resetColour)
-	fmt.Println("")
-	fmt.Println(bgYellow + textBoldBlack)
-	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
-	fmt.Println(" □                                                                                       □ ")
-	fmt.Println(" □   Add the two-factor authentication (2FA) secret key generated below to another app   □ ")
-	fmt.Println(" □                                                                                       □ ")
-	fmt.Println(" □      MFA View aims to be as secure as possible it requires another app to provide     □ ")
-	fmt.Println(" □                    a two-factor authentication (2FA) code to login.                   □ ")
-	fmt.Println(" □   The two-factor authentication (2FA) secret key can be changed later in mfaview.env  □ ")
-	fmt.Println(" □                                                                                       □ ")
-	fmt.Println(" □                            (Type \"exit\" to quit program)                            □ ")
-	fmt.Println(" □                                                                                       □ ")
-	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
+	fmt.Print(resetColour)
+	fmt.Println(textBoldBlack)
+	fmt.Printf("    Please enter a password between 20-100\n    characters: ")
+	var addPassword string
+	fmt.Scan(&addPassword)
+	fmt.Println(resetColour)        	
+	validationAddPassword := validateInput(addPassword, "password")
+	if addPassword == "exit" || addPassword == "Exit" || addPassword == "EXIT" {
+		exitProgramCli()
+	} else if validationAddPassword == false {
+		invalidInputCli()
+		addPasswordCli()	
+	} else {
+	// Test that will be replaced later
+	fmt.Println("okay")
+	}
+}	
+
+// A recursive function to add a 2FA secret key, MFA View needs a username(email), password and 2FA code to login to view other 2FA/MFA codes
+// This function still needs work:
+func add2faCli() {
+	clearScreen()
+	typeExitCli()
+	messageBoxCli(bgYellow, textBoldWhite, "Scan the QR (Quick Response) code with another authenticator app or copy the secret key to another authenticator app ")
+	fmt.Println(bgRed + textBoldWhite)
+	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
+	fmt.Println(" □                                                                                                   □ ")
+	fmt.Println(" □                  MFA View aims to be as secure as possible, it requires another                   □ ")
+	fmt.Println(" □           authenticator app to provide a 2FA (Two-Factor Authentication) code to login.           □ ")
+	fmt.Println(" □    The 2FA secret key can be manually changed later in /usr/local/etc/mfaview/env/mfaview.env.    □ ")
+	fmt.Println(" □                                                                                                   □ ")
+	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
 	fmt.Println(resetColour)
 	fmt.Scan()
+}
 
+// Basic function with no parameters to call the addEmailCli, addPasswordCli, add2faCli functions
+func createUserCli() {
+	addEmailCli()
+	addPasswordCli()
+	add2faCli()	
 }
 
 // Recursive function to change an existing known password
-func changePassword() {
+// This function needs alot of work:
+func changePasswordCli() {
 	clearScreen()
 	fmt.Println(bgRed + textBoldWhite)
 	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
 	fmt.Println(" □                                                                   □ ")
-	fmt.Println(" □      The change password option is set to yes in mfaview.env      □ ")
+	fmt.Println(" □      The change_password option is set to yes in mfaview.env      □ ")
 	fmt.Println(" □                                                                   □ ")
 	fmt.Println(" □    To change the password the exisitng password must be known     □ ")
 	fmt.Println(" □                                                                   □ ")
-	fmt.Println(" □                  (Type \"exit\" to quit program)                  □ ")
+	fmt.Println(" □                  (Type \"exit\" to quit program)                    □ ")
 	fmt.Println(" □                                                                   □ ")
 	fmt.Println(" □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ ")
 	fmt.Println(resetColour)
 }
 
+// Declare two string variables for HTML
 var startHTML string
 var endHTML string
 
+// Main function
 func main() {
 	err := godotenv.Load("/usr/local/etc/mfaview/env/mfaview.env")
 	if err != nil {
 		panic("Error loading mfaview.env file")
 	}
 
-	email := os.Getenv("email")
-	password := os.Getenv("password")
-	address := os.Getenv("address")
-	port := os.Getenv("port")
-	addAccount := os.Getenv("add_account")
+	envEmail := os.Getenv("email")
+	envPassword := os.Getenv("password")
+	envChangePassword := os.Getenv("change_password")
+	envAddress := os.Getenv("address")
+	envPort := os.Getenv("port")
+	envAddAccount := os.Getenv("add_account")
 
-	if email == "" && password == "" {
-		createUser()
-	} else {
+	validationEnvEmail := validateInput(envEmail, "email")
+	
+	envPortInt, err := strconv.Atoi(envPort)
+	if err != nil {
+		invalidEnvCli("Port must be a number in /usr/local/etc/mfaview/mfaview.env")
+	}
+
+	if envEmail == "" && envPassword == "" {
+		createUserCli()
+	} else if validationEnvEmail == false {
+		invalidEnvCli("Email address stored in /usr/local/etc/mfaview.env is invalid")
+	} else if envAddress == "" {
+		
+	
+	} else if envPortInt <= 0 || envPortInt >= 65536 {
+	
+	} else if envChangePassword == "yes" || envChangePassword == "Yes" || envChangePassword == "YES" {
+		changePasswordCli()
 
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
@@ -223,11 +340,11 @@ func main() {
 			input2fa := r.FormValue("2FA")
 
 			// Validate email address, password and 2FA
-			validationEmail := validateInput(inputEmail, "email", "6", "320")
-			validationPassword := validateInput(inputPassword, "", "20", "100")
-			validation2fa := validateInput(input2fa, "number", "6", "6")
+			validationEmail := validateInput(inputEmail, "email")
+			validationPassword := validateInput(inputPassword, "password")
+			validation2fa := validateInput(input2fa, "2FA")
 
-			// Need to work on validation more
+			// Conditional statement to validate input
 			if inputEmail == "" && inputPassword == "" && input2fa == "" {
 			} else if validationEmail == false {
 				textBox(w, "Please enter a valid email address, max 100 charecters length")
@@ -244,7 +361,9 @@ func main() {
 			fmt.Fprint(w, endHTML)
 		})
 
-		if addAccount == "yes" || addAccount == "Yes" || addAccount == "YES" {
+		// Conditional statement to activate the add account section of the website based on value inside the
+		// mfaview.env configuration file
+		if envAddAccount == "yes" || envAddAccount == "Yes" || envAddAccount == "YES" {
 
 			http.HandleFunc("/add-account", func(w http.ResponseWriter, r *http.Request) {
 				if err := r.ParseForm(); err != nil {
@@ -277,7 +396,7 @@ func main() {
 				fmt.Fprintf(w, "</form>")
 				fmt.Fprintf(w, "</div")
 
-				// Get email address, password and 2FA from HTTP POST
+				// Get email address, password, MFA account name, MFA secret key, SHA type and 2FA from HTTP POST
 				inputEmail := r.FormValue("email")
 				inputPassword := r.FormValue("password")
 				inputAccount := r.FormValue("account")
@@ -285,14 +404,15 @@ func main() {
 				inputSha := r.FormValue("sha")
 				input2fa := r.FormValue("2FA")
 
-				// Validate email address, password and 2FA
-				validationEmail := validateInput(inputEmail, "email", "6", "320")
-				validationPassword := validateInput(inputPassword, "", "20", "100")
-				validationAccount := validateInput(inputAccount, "", "20", "100")
-				validationMfa := validateInput(inputMfa, "", "10", "200")
+				// Returns false or true based on if input is valid
+				validationEmail := validateInput(inputEmail, "email")
+				validationPassword := validateInput(inputPassword, "password")
+				validationAccount := validateInput(inputAccount, "text")
+				validationMfa := validateInput(inputMfa, "text")
 				validationSha := slices.Contains(shaList, inputSha)
-				validation2fa := validateInput(input2fa, "number", "6", "6")
+				validation2fa := validateInput(input2fa, "2FA")
 
+				// Conditional statement to validate input
 				if inputEmail == "" && inputPassword == "" && inputAccount == "" && inputMfa == "" && inputSha == "" && input2fa == "" {
 				} else if validationEmail == false {
 					textBox(w, "Please enter a valid email address, max 100 charecters length")
@@ -313,6 +433,7 @@ func main() {
 			})
 
 		} else {
+			// HTTP server informing the user add account is not available
 			http.HandleFunc("/add-account", func(w http.ResponseWriter, r *http.Request) {
 				if err := r.ParseForm(); err != nil {
 					fmt.Fprintf(w, "ParseForm() err: %v", err)
@@ -327,7 +448,8 @@ func main() {
 		}
 	}
 
-	socket := address + ":" + port
+	// IP address and port number, value taken from mfaview.env
+	socket := envAddress + ":" + envPort
 	fmt.Println("MFA View is running on: " + socket)
 
 	// Start server on port specified above
